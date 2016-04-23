842761d2c89af9f2882c9d05154006ca3cdde889
Richard Stiehm <richardstiehm@gmail.com>
2016-04-23 12:16:32 -0400

    Basic Setup With Packages

--- app.js
+++ app.js
@@ -4,15 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var monggose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup----------------------------------------
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +30,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//EXPRESS SESSION---------------------------------
+app.use(session({
+  secret: 'Once Again Harry Wins The Cutest Dog Award',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//PASSPORT---------------------------------------
+app.use(passport.initialize());
+app.use(passport.session());
+
+//EXPRESS VALIDATOR------------------------------
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//CONNECT-FLASH------------------------------
+app.use(flash());
+
+//GLOBAL VARS--------------------------------
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 app.use('/', routes);
 app.use('/users', users);
