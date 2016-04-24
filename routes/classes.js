b4fec9e345eb0f84eb8b2d5da5dfb4c7af307d86
moondogg71 <richardstiehm@gmail.com>
2016-04-24 16:18:54 +0000

    Working View Lesson

--- routes/classes.js
+++ routes/classes.js
@@ -19,4 +19,27 @@ router.get('/:id/details', function(req, res, next) {
 	});
 });
 
+router.get('/:id/lessons', function(req, res, next) {
+	Class.getClassById([req.params.id],function(err, classname){
+		if(err) throw err;
+		res.render('classes/lessons', { class: classname });
+	});
+});
+
+
+router.get('/:id/lessons/:lesson_id', function(req, res, next) {
+	Class.getClassById([req.params.id],function(err, classname){
+		var lesson;
+		if(err) throw err;
+		for(i=0;i<classname.lessons.length;i++){
+			if(classname.lessons[i].lesson_number == req.params.lesson_id){
+				lesson = classname.lessons[i];
+			}
+		}
+		res.render('classes/lesson', { class: classname,lesson: lesson });
+	});
+});
+
+
+
 module.exports = router;
